842b21f15a8f7bb04efa9e74cb244cf70444e68c
Lubna <muspaksamsu@gmail.com>
2024-07-08 13:45:10 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+// Think of at least three kinds of your favorite pizza. Store these pizza names in a array, 
+//and then use a for loop to print the name of each pizza.
+var pizza = ["Tikka", "BBQ", "Fagita"];
+for (var _i = 0, pizza_1 = pizza; _i < pizza_1.length; _i++) {
+    var onePizza = pizza_1[_i];
+    console.log("I like ".concat(onePizza, " pizza"));
+}
+console.log("pizza is love");
---
+++ main.ts
@@ -0,0 +1,9 @@
+// Think of at least three kinds of your favorite pizza. Store these pizza names in a array, 
+//and then use a for loop to print the name of each pizza.
+
+let pizza = ["Tikka" , "BBQ" , "Fagita"]
+for (let onePizza of pizza){
+    console.log(`I like ${onePizza} pizza`);
+    }
+    console.log("pizza is love");
+    
